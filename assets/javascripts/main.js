2e6ed1050c2fa5f870a5fdb076e201a6bce322d0
nestklim <nestklim@mail.ru>
2017-04-11 00:50:17 +0300

    draft

---
+++ assets/javascripts/main.js
@@ -0,0 +1,35 @@
+;(function($) {
+	"use strict";
+
+/*	$(window).load(function() {
+
+		var portfolioGrid = $('.ba-portfolio-grid');
+
+		portfolioGrid.isotope({
+	  		itemSelector: '.ba-portfolio-grid__item'
+		});
+
+		var filterLinks = $('.ba-portfolio-filter a');
+
+		filterLinks.on('click', function(e){
+			e.preventDefault();
+			var filterClass = $(this).data('filter');
+
+			filterLinks.removeClass('active');
+			$(this).addClass('active');
+
+			portfolioGrid.isotope({ filter: filterClass })
+		});
+
+	});*/
+
+/*	$('.ba-slider').slick({
+
+		arrows: false,
+		dots: true
+
+	});*/
+
+
+})(jQuery);
+
